45f0310dd489ff8eba987482e3b24ee4744934a7
andrey_mardash <andrey.mardash@gmail.com>
2019-08-25 10:53:49 +0300

    gitignore and package.json

--- scripts.js
+++ scripts.js
@@ -1,51 +1,51 @@
 let arrNumbers = [];
 let arrOperations = [];
 
-function number(x){
-    let str = document.getElementById('input').value;
-    if(str === '0' && x === 0) x = '';
-    document.getElementById('input').value += x;
-    console.log( document.getElementById('input').value);
+function number (x) {
+  const str = document.getElementById('input').value;
+  if (str === '0' && x === 0) x = '';
+  document.getElementById('input').value += x;
+  console.log(document.getElementById('input').value);
 }
 
-function operation(x){
-    arrNumbers.push( +document.getElementById('input').value);
-    document.getElementById('input').value = '';
-    arrOperations.push(x);
-    console.log(x);
-    console.log(arrNumbers);
+function operation (x) {
+  arrNumbers.push(+document.getElementById('input').value);
+  document.getElementById('input').value = '';
+  arrOperations.push(x);
+  console.log(x);
+  console.log(arrNumbers);
 }
 
-function result(){
-    arrNumbers.push(+document.getElementById('input').value);
-    let count = arrNumbers[0];
-    arrOperations.map((el,ind) => {
-        if(el === '+') count += arrNumbers[ind + 1];
-        if(el === '-') count -= arrNumbers[ind + 1];
-        if(el === 'x') count *= arrNumbers[ind + 1];
-        if(el === '/') count /= arrNumbers[ind + 1];
-    });
-    document.getElementById('input').value = count;
-    arrNumbers = [];
-    arrOperations = [];
+function result () {
+  arrNumbers.push(+document.getElementById('input').value);
+  let count = arrNumbers[0];
+  arrOperations.map((el, ind) => {
+    if (el === '+') count += arrNumbers[ind + 1];
+    if (el === '-') count -= arrNumbers[ind + 1];
+    if (el === 'x') count *= arrNumbers[ind + 1];
+    if (el === '/') count /= arrNumbers[ind + 1];
+  });
+  document.getElementById('input').value = count;
+  arrNumbers = [];
+  arrOperations = [];
 }
 
-function clearInput(){
-    arrNumbers = [];
-    arrOperations = [];
-    document.getElementById('input').value = '';
+function clearInput () {
+  arrNumbers = [];
+  arrOperations = [];
+  document.getElementById('input').value = '';
 }
 
-function del(){
-    let str = document.getElementById('input').value;
-    document.getElementById('input').value = str.slice(0, str.length-1);
-    console.log('delete');
+function del () {
+  const str = document.getElementById('input').value;
+  document.getElementById('input').value = str.slice(0, str.length - 1);
+  console.log('delete');
 }
 
-function changeMinus(){
-    const str = document.getElementById('input').value;
-    if(str !== '' && str !== '0'){
-        str[0] === '-' ? document.getElementById('input').value = str.slice(1)
-                       : document.getElementById('input').value = `-${str}`;
-    }
+function changeMinus () {
+  const str = document.getElementById('input').value;
+  if (str !== '' && str !== '0') {
+    str[0] === '-' ? document.getElementById('input').value = str.slice(1)
+      : document.getElementById('input').value = `-${str}`;
+  }
 }
